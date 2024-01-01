3d92492de2970b9fb055ebd6c7916bd0804244b5
Nishant Dutta <nishant.dutta@sas.com>
2024-01-01 12:58:20 +0530

    Added level order for calculating height

--- src/main/java/org/algorithms/tree/binarytree/Height.java
+++ src/main/java/org/algorithms/tree/binarytree/Height.java
@@ -1,5 +1,7 @@
 package org.algorithms.tree.binarytree;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 // WAP to find the height/depth of a Binary Tree(might be incomplete binary tree i.e. having
@@ -10,17 +12,18 @@ public class Height {
         BinaryTree binaryTree = new BinaryTree(new BinaryTreeNode(1));
 
         // Adding nodes
-        binaryTree.root.left = new BinaryTreeNode(2);
-        binaryTree.root.right = new BinaryTreeNode(3);
-        binaryTree.root.left.left = new BinaryTreeNode(4);
-        binaryTree.root.left.right = new BinaryTreeNode(5);
-        binaryTree.root.right.left = new BinaryTreeNode(6);
-        binaryTree.root.right.right = new BinaryTreeNode(7);
-        binaryTree.root.right.right.left = new BinaryTreeNode(8);
+//        binaryTree.root.left = new BinaryTreeNode(2);
+//        binaryTree.root.right = new BinaryTreeNode(3);
+//        binaryTree.root.left.left = new BinaryTreeNode(4);
+//        binaryTree.root.left.right = new BinaryTreeNode(5);
+//        binaryTree.root.right.left = new BinaryTreeNode(6);
+//        binaryTree.root.right.right = new BinaryTreeNode(7);
+//        binaryTree.root.right.right.left = new BinaryTreeNode(8);
 
         Height height = new Height();
         System.out.println("Recursively: " + height.findHeightRecursively(binaryTree.root));
         System.out.println("Non-recursively: " + height.findHeightNonRecursively(binaryTree.root));
+        System.out.println("Using Level-order: " + height.findHeightUsingLevelOrder(binaryTree.root));
     }
 
     public int findHeightRecursively(BinaryTreeNode node){
@@ -35,8 +38,6 @@ public class Height {
     }
 
 //    uses post-order traversal and tracks stack size(easy approach)
-//    Alternatively, we can also use level-order traversal and push null into queue for
-//    each level(bit complex, present in Karumanchi)
     public int findHeightNonRecursively(BinaryTreeNode node){
         int maxHeight = 0;
         Stack<BinaryTreeNode> stack = new Stack<>();
@@ -60,5 +61,35 @@ public class Height {
             }
         }while (!stack.isEmpty());
         return maxHeight;
+    }
+
+//    we can also use level-order traversal and push null into queue for
+//      each level(bit complex, present in Karumanchi)
+    public int findHeightUsingLevelOrder(BinaryTreeNode node){
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        int level = 0;
+
+        if(node != null){
+            queue.add(node);
+            queue.add(null);
+
+            while (!queue.isEmpty()){
+                node = queue.poll();
+                if(node == null){
+                    if(!queue.isEmpty()){
+                        queue.add(null);
+                    }
+                    level++;
+                }else{
+                    if(node.left != null)
+                        queue.add(node.left);
+
+                    if(node.right != null){
+                        queue.add(node.right);
+                    }
+                }
+            }
+        }
+        return level;
     }
 }
